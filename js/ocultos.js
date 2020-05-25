e531a83bf75127699f991603cd46b3e1a46c052b
sebastianllata <sebastianllata@upeu.edu.pe>
2020-05-25 13:34:01 -0500

    vol5casiqueda

--- js/ocultos.js
+++ js/ocultos.js
@@ -1,5 +1,5 @@
 var montotal = [];
-var fondobank = [{
+var fondobank = [{//1 arreglo con un objeto
     montoinicialsoles: 1000,
     montoinicialdolar: 800
 }];
@@ -54,36 +54,44 @@ function mostrarotrascant() {
     document.getElementById("otrascantidades").style.display ="block";
 }
 
-function imprimvalor(p) {
-    var monto = p.value;
+function imprimvalor(p) {//variable p = this llamar elemento completo
+    var saldo = p.value;
     console.log(p);
-    if(p.name == "sol" ){
-        var objeto = {
-            tipo:"sol",
-            monto:monto
-            };
-        montotal.push(objeto);//agregar al array
-        document.getElementById("spam").value = "S/."+montotal[0].monto;
-        retiotrascantidadessoles();
-    }
-    else{
-        var objeto = {
-            tipo:"dolar",
-            monto:monto
-            };
-        montotal.push(objeto);
-        document.getElementById("spam").value = "$/."+montotal[0].monto;
-        retiotrascantidadesdolar();
-    }
+
+        if(p.name == "sol" ){
+            var objeto = {
+                tipo:"sol",
+                monto:saldo
+                };
+            montotal.push(objeto);//agregar al array
+            document.getElementById("spam").value = "S/."+montotal[0].monto;//monto es el saldo que entro como variable
+            retiotrascantidadessoles();
+        }
+        else{
+            var objeto = {
+                tipo:"dolar",
+                monto:saldo
+                };
+                
+            montotal.push(objeto);
+            document.getElementById("spam").value = "$/."+montotal[0].monto;
+            retiotrascantidadesdolar();
+        }
+    
 }
 function corregir() {
     montotal=[];//limpia el array
     //console.log(montotal);
-    document.getElementById("spam").value = montotal;
+    document.getElementById("spam").value ="";
     retiotrascantidadesdolar();
 }
 function retiotrascantidadesdolar() {
     var caja = document.getElementById("otrascantidades");
+    var fg ={
+        tipo:"otrosdolar",
+        monto:0
+    };
+    montotal.push(fg);
     if (caja.style.display == "none") {
         ocualtarretirodolares();
         mostrarotrascant();
@@ -96,6 +104,11 @@ function retiotrascantidadesdolar() {
 }
 function retiotrascantidadessoles() {
     var caja = document.getElementById("otrascantidades");
+    var fg ={
+        tipo:"otrossoles",
+        monto:0
+    };
+    montotal.push(fg);
     if (caja.style.display == "none") {
         ocultarretirosoles();
         mostrarotrascant();
@@ -105,17 +118,43 @@ function retiotrascantidadessoles() {
     }
 }
 function confirmar() {
-    //console.log(montotal);
+    console.log(montotal);
     //console.log(fondobank);
-    if (montotal[0].tipo == "sol") {
-        fondobank[0].montoinicialsoles = fondobank[0].montoinicialsoles-montotal[0].monto;
-        document.getElementById("saldo").value = fondobank[0].montoinicialsoles;    
-    }
-    else {
-        fondobank[0].montoinicialdolar = fondobank[0].montoinicialdolar-montotal[0].monto;
-        document.getElementById("saldo").value = fondobank[0].montoinicialdolar;
-    }
-    ultimatrans();
+        switch (montotal[0].tipo) {
+            case "sol":
+                console.log("xfasfs");
+                fondobank[0].montoinicialsoles = fondobank[0].montoinicialsoles-montotal[0].monto;
+                document.getElementById("saldo").value = fondobank[0].montoinicialsoles;
+                ultimatrans();
+                break;
+            case "dolar":
+                fondobank[0].montoinicialdolar = fondobank[0].montoinicialdolar-montotal[0].monto;
+                document.getElementById("saldo").value = fondobank[0].montoinicialdolar;
+                ultimatrans();
+                break;
+            case "otrossoles":
+                montotal[0].monto = parseFloat( document.getElementById("spam").value);
+                if (montotal[0].monto > fondobank[0].montoinicialsoles) {
+                     alert("el monto exede sus fondos");
+                }else{
+                    fondobank[0].montoinicialsoles = fondobank[0].montoinicialsoles-montotal[0].monto;
+                    document.getElementById("saldo").value = fondobank[0].montoinicialsoles;
+                    ultimatrans();
+                }
+                break;
+            case "otrosdolar":
+                    montotal[0].monto = parseFloat( document.getElementById("spam").value);
+                    if (montotal[0].monto > fondobank[0].montoinicialdolar) {
+                        alert("el monto exede sus fondos");
+                    }else{
+                        fondobank[0].montoinicialdolar = fondobank[0].montoinicialdolar-montotal[0].monto;
+                        document.getElementById("saldo").value = fondobank[0].montoinicialdolar;
+                        ultimatrans();
+                    }
+                break;
+            default:
+                break;
+        }
 
 }
 //
